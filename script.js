0c8c09e8695dff8ebb9726c7fd73e7f9e3cc86b0
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-14 17:06:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Person {}
+class Person {
+	  constructor(name, age) {
+    this._name = name; 
+    this._age = age;   
+  }
+	 get name() {
+    return this._name;
+  }
+	  set age(newAge) {
+    this._age = newAge;
+  }
+	  get age() {
+    return this._age;
+  }
+}
 
-class Student extends Person {}
+class Student extends Person {
+	 constructor(name, age) {
+    super(name, age);
+}
+	 study() {
+    console.log(`${this.name} is studying`);
+  }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	  constructor(name, age) {
+    super(name, age);
+  }
+	  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
